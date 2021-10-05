22215516a539e3af741e7c925fa65813ca978198
ncinsli <56553344+ncinsli@users.noreply.github.com>
2021-10-05 19:19:19 +0500

    Code style fix
    + FIxed imports
    + Rebuilt package-lock

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
-import * as express from 'express';
-import {Request, Response} from 'express';
-import * as dotenv from 'dotenv';
-import * as mysql from 'mysql2';
-import * as cors from 'cors';
+import { Request, Response } from 'express';
+import express from 'express';
+import dotenv from 'dotenv';
+import mysql from 'mysql2';
+import cors from 'cors';
+
 dotenv.config();
+
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
@@ -11,77 +13,109 @@ const connection = mysql.createConnection({
   database: process.env.DB_NAME,
 });
 connection.connect();
+
 const app = express();
 const port = process.env.PORT;
+
 app.use(express.json());
 app.use(express.urlencoded());
+
 app.use(cors({
     origin: '*'
 }));
-app.get('/', (req:Request, res:Response) => {
+
+app.get('/', (req: Request, res: Response) => {
     res.send("Hello world!");
 })
-export interface post{
-    id: Number,
-    title: String,
-    date: String,
-    body: String,
-    attachments: String,
+
+export interface IPost{
+    id: number,
+    title: string,
+    date: string,
+    body: string,
+    attachments: string,
+    titlePicture : string
 }
-app.get('/posts/get/:from', (req:Request, res:Response) => {
-    if(!Number.isInteger(Number(req.params.from))) res.json({error: "from_invalid"});
-    if(Number(req.params.from)<0) res.json({error: "from_invalid"});
+
+app.get('/posts/get/:from', (req: Request, res: Response) => {
+    
+    if (!Number.isInteger(Number(req.params.from))) 
+        res.json({error: "from_invalid"});
+    
+    if (Number(req.params.from) < 0) 
+        res.json({error: "from_invalid"});
+    
     connection.promise().query("SELECT * FROM `posts` ORDER BY `id` LIMIT ?,?", [Number(req.params.from),Number(req.params.from+20)]).then(([rows]) => {
         res.json(rows)
     }).catch(console.log);
 })
-app.get('/post/get/:id', (req:Request, res:Response) => {
-    if(!Number.isInteger(Number(req.params.id))) res.json({error: "id_invalid"});
-    if(Number(req.params.id)<=0) res.json({error: "id_invalid"});
-    connection.promise().query("SELECT * FROM `posts` WHERE `id`=?", [Number(req.params.id)]).then(([rows]) => {
-        let log = JSON.parse(JSON.stringify(rows))[0];
-        if(typeof log == "undefined") res.json({error: "id_invalid"});
-        res.json(log)
+
+app.get('/post/get/:id', (req: Request, res: Response) => {
+    
+    if (!Number.isInteger(Number(req.params.id))) 
+        res.json({error: "id_invalid"});
+
+    if (Number(req.params.id) <= 0) 
+        res.json({error: "id_invalid"});
+        connection.promise().query("SELECT * FROM `posts` WHERE `id`=?", [Number(req.params.id)]).then(([rows]) => {
+            let log = JSON.parse(JSON.stringify(rows))[0];
+            if (typeof log == "undefined") 
+                res.json({error: "id_invalid"});
+            res.json(log)
     }).catch(console.log);
-})
-app.post('/post/add/:token/', (req:Request, res:Response) => {
+});
+
+app.post('/post/add/:token/', (req: Request, res: Response) => {
     connection.promise().query("SELECT * FROM `users` WHERE `token`=?", [req.params.token]).then(([rows]) => {
-        if(!JSON.parse(JSON.stringify(rows))[0].hasOwnProperty('token')){
+        
+        if (!JSON.parse(JSON.stringify(rows))[0].hasOwnProperty('token'))
             res.json({error: "token_invalid"});
-        }
-        let now:String = new Date().toLocaleString();
-        connection.promise().query("INSERT INTO `posts` (`title`,`date`,`body`,`attachments`) VALUES (?,?,?,?)", [req.body.title,now,req.body.body,req.body.attachments]).then(([rows]) => {
+        
+        let now : string = new Date().toLocaleString();
+        connection.promise().query("INSERT INTO `posts` (`title`,`date`,`body`,`attachments`) VALUES (?,?,?,?)", [req.body.title, now, req.body.body, req.body.attachments]).then(([rows]) => {
             res.send('""');
             res.status(201).end();
         }).catch(console.log);
     }).catch(console.log);
 })
-app.post('/post/edit/:token/:id', (req:Request, res:Response) => {
+
+app.post('/post/edit/:token/:id', (req: Request, res: Response) => {
     connection.promise().query("SELECT * FROM `users` WHERE `token`=?", [req.params.token]).then(([rows]) => {
-        if(!JSON.parse(JSON.stringify(rows))[0].hasOwnProperty('token')){
+        
+        if (!JSON.parse(JSON.stringify(rows))[0].hasOwnProperty('token'))
             res.json({error: "token_invalid"});
-        }
-        if(!Number.isInteger(Number(req.params.id))) res.json({error: "id_invalid"});
-        if(Number(req.params.id)<=0) res.json({error: "id_invalid"});
-        connection.promise().query("UPDATE `posts` SET `title`=?, `body`=?, `attachments`=? WHERE `id`=?", [req.body.title,req.body.body,req.body.attachments,Number(req.params.id)]).then(([rows]) => {
+
+        if (!Number.isInteger(Number(req.params.id))) 
+            res.json({error: "id_invalid"});
+        if (Number(req.params.id) <= 0) 
+            res.json({error: "id_invalid"});
+        
+        connection.promise().query("UPDATE `posts` SET `title`=?, `body`=?, `attachments`=? WHERE `id`=?", [req.body.title, req.body.body, req.body.attachments, Number(req.params.id)]).then(([rows]) => {
             res.send('""');
             res.status(202).end();
         }).catch(console.log);
     }).catch(console.log);
 })
-app.post('/post/del/:token/:id', (req:Request, res:Response) => {
+
+app.post('/post/del/:token/:id', (req: Request, res: Response) => {
     connection.promise().query("SELECT * FROM `users` WHERE `token`=?", [req.params.token]).then(([rows]) => {
-        if(!JSON.parse(JSON.stringify(rows))[0].hasOwnProperty('token')){
+       
+        if (!JSON.parse(JSON.stringify(rows))[0].hasOwnProperty('token'))
             res.json({error: "token_invalid"});
-        }
-        if(!Number.isInteger(Number(req.params.id))) res.json({error: "id_invalid"});
-        if(Number(req.params.id)<=0) res.json({error: "id_invalid"});
+    
+        if (!Number.isInteger(Number(req.params.id))) 
+            res.json({error: "id_invalid"});
+
+        if (Number(req.params.id) <= 0) 
+            res.json({error: "id_invalid"});
+
         connection.promise().query("DELETE FROM `posts` WHERE `id`=?", [Number(req.params.id)]).then(([rows]) => {
             res.send('""');
             res.status(202).end();
         }).catch(console.log);
     }).catch(console.log);
 })
+
 app.listen(port, () => {
     console.log(`⚡️The application is listening on port ${port}!⚡️`);
 })
